25e34794e45e9d4a861239165089abdd9a18358b
parneet95 <parneet.995@gmail.com>
2016-03-28 19:41:33 -0400

    Basic setup of the website complete.

---
+++ components/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import RaisedButton from 'material-ui/lib/raised-button';
+import {SideNav} from './sideNav.jsx';
+
+import './../less/main.less'
+
+class Main extends React.Component {
+  
+    render() {
+        return (
+            <div className="main-div">
+                
+                <SideNav docked={false} />
+                
+                <RaisedButton label="Default" />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('content'));
